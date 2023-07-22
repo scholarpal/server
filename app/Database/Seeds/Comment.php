5886965ed4be553a48abec053196615302ebf1b1
Rizal Anditama <rizalanditama2105@gmail.com>
2023-07-23 00:20:42 +0700

    added another seeder

--- app/Database/Seeds/Comment.php
+++ app/Database/Seeds/Comment.php
@@ -11,39 +11,14 @@ class Comment extends Seeder
         $faker = \Faker\Factory::create();
 
         $dataComment = [
-            'category_id' =>
-            $faker->numberBetween(1, 3),
-            'rating_id' =>
-            $faker->numberBetween(1, 5),
-            'user_id'
-            => $faker->numberBetween(1, 5),
+            'category_id' => $faker->numberBetween(1, 3),
+            'rating_id' => $faker->numberBetween(1, 5),
+            'user_id' => $faker->numberBetween(1, 5),
             'comment' => $faker->sentence
         ];
 
-        $dataRating = [
-            'facility' => $faker->numberBetween(1, 5),
-            'food' => $faker->numberBetween(1, 5),
-            'happiness' => $faker->numberBetween(1, 5),
-        ];
-
-        $username = $faker->userName();
-        $dataUser = [
-            'username' => $username,
-            'email' => $faker->email(),
-            'phone_number' => rand(111111111111, 999999999999),
-            'otp_code' => null,
-            'otp_code_exp' => null,
-            'school' => 'SMKN 12 Jakarta',
-            'avatar' => "avatars.dicebear.com/api/open-peeps/$username.svg",
-            'password' => $faker->password(),
-        ];
-
         for ($i = 0; $i < 100; $i++) {
-            $data[$i] = $dataUser;
-            // $this->db->table('rating')->insert($dataRating);
-            // $this->db->table('user')->insert($dataUser);
-            // $this->db->table('comments')->insert($dataComment);
+            $this->db->table('comments')->insert($dataComment);
         }
-        
     }
 }
